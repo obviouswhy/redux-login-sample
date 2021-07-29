c4cfd4cff5496ccaa17b553da189f698738a1cb2
obviouswhy <sergilockerz@gmail.com>
2021-07-29 17:14:53 +0900

    added user and password label

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -14,57 +14,60 @@ export const Login = () => {
       <div className={'row'}>
         <h1 className={'title'}>ログイン画面</h1>
       </div>
-      <>
-        <div className={'row'}>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'flex-start',
-              width: '20rem',
-              flexDirection: 'column',
-            }}>
-            <label className={'textbox-label'}>ユーザー名</label>
-            <input
-              className={'textbox'}
-              aria-label="Username Input"
-              value={user.username}
-              placeholder={'admin'}
-              onChange={e =>
-                setUser(prevState => ({
-                  ...prevState,
-                  username: e.target.value,
-                }))
-              }
-            />
-            <label className={'textbox-label'}>パスワード</label>
-            <input
-              className={'textbox'}
-              type={'password'}
-              aria-label="Password Input"
-              placeholder={'**********'}
-              value={user.password}
-              onChange={e =>
-                setUser(prevState => ({
-                  ...prevState,
-                  password: e.target.value,
-                }))
-              }
-            />
-          </div>
+      <div className={'row'}>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'flex-start',
+            width: '20rem',
+            flexDirection: 'column',
+          }}>
+          <label className={'textbox-label'}>ユーザー名</label>
+          <input
+            className={'textbox'}
+            aria-label="Username Input"
+            value={user.username}
+            placeholder={'admin'}
+            onChange={e =>
+              setUser(prevState => ({
+                ...prevState,
+                username: e.target.value,
+              }))
+            }
+          />
+          <label className={'textbox-label'}>パスワード</label>
+          <input
+            className={'textbox'}
+            type={'password'}
+            aria-label="Password Input"
+            placeholder={'**********'}
+            value={user.password}
+            onChange={e =>
+              setUser(prevState => ({
+                ...prevState,
+                password: e.target.value,
+              }))
+            }
+          />
         </div>
-        <div className={'row'}>
-          <button
-            className={'button'}
-            aria-label="Log In"
-            onClick={() =>
-              dispatch(authUser(user)).then(res =>
-                res ? history.push('/private') : null,
-              )
-            }>
-            ログイン
-          </button>
-        </div>
-      </>
+      </div>
+      <div className={'row'}>
+        <button
+          className={'button'}
+          aria-label="Log In"
+          onClick={() =>
+            dispatch(authUser(user)).then(res =>
+              res ? history.push('/private') : null,
+            )
+          }>
+          ログイン
+        </button>
+      </div>
+      <div className={'row'}>
+        <label className={'small-label'}>
+          (ユーザー名はadminです。パスワードもadminです)
+        </label>
+      </div>
     </div>
   )
 }
